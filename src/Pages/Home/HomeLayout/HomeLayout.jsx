eee133fe82b2270e78e1625c8468eb61b90ff832
Chowon-Hasan <chowonhasan7@gmail.com>
2023-06-07 03:31:41 +0600

    added footer

--- src/Pages/Home/HomeLayout/HomeLayout.jsx
+++ src/Pages/Home/HomeLayout/HomeLayout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../../Shared/Header/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "../../Shared/Footer/Footer";
 
 const HomeLayout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <div className="py-52 my-16">
+        <Outlet />
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
